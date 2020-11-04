7a48b37835f7081f3da9f9f39f3288fc0e8a5bd4
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-11-04 12:54:18 +0100

    Remove unused error decoding
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- internal/errors.go
+++ internal/errors.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"go.uber.org/cadence"
@@ -38,37 +37,6 @@ func EncodeError(err error) error {
 		}
 
 		return cadence.NewCustomError("AWS_SDK_"+aerr.Code(), newErr)
-	}
-
-	return err
-}
-
-// decodedError is a serialized version of the AWS SDK error.
-type decodedError struct {
-	Code    string
-	Message string
-	OrigErr error
-}
-
-// decodedError implements the error interface.
-func (e decodedError) Error() string {
-	return fmt.Sprintf("%s: %s", e.Code, e.Message)
-}
-
-// DecodeError decodes an error returned by a Cadence activity.
-// If the error is an AWS SDK error, it gets converted to an AWS SDK compatible error interface.
-// Otherwise the original error is returned.
-func DecodeError(err error) error {
-	if err == nil {
-		return nil
-	}
-
-	if customError, ok := err.(*cadence.CustomError); ok && strings.HasPrefix(customError.Reason(), "AWS_SDK_") {
-		var aerr decodedError
-
-		_ = customError.Details(&aerr)
-
-		return awserr.New(aerr.Code, aerr.Message, aerr.OrigErr)
 	}
 
 	return err
